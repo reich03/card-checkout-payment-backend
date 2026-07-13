89bdf5fbd327271f93d9be65670a27d061c48cc7
JhojanGrisales <redjhojan0319@gmail.com>
2026-07-13 08:17:50 -0500

    fixed run port definition

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
     useGlobalPrefix: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
